8da6eb20fe196a9f8490380a86eb5370da2773c2
maheshwarishikha <maheshwari.shikha@gmail.com>
2021-12-12 00:30:53 +0530

    Add files via upload

--- __tests__/unit/calculator.tests.js
+++ __tests__/unit/calculator.tests.js
@@ -1,4 +1,5 @@
 const Calculator = require('../../calculator/calc');
+const axios = require('axios');
 
 it ("calculator function should exist", () => {
     expect( typeof(Calculator.performOperation)).toBe ("function");
@@ -17,19 +18,23 @@ it ("calculator function should understand 'add', 'sub', 'mult' and 'div' operat
 });
 
 it ("'add' function should return addition of input parameters", async () => {
-    expect( await Calculator.performOperation('add', 'I,IV,X,XX')).toBe (35);
+    // jest.mock('axios');
+    // const resp = {data: [1, 4, 10, 20]};
+    // axios.get.mockResolvedValue(resp);
+    expect( await Calculator.performOperation('add', 'I,IV,X,XX')).toBe ("XXXV");
 });
 
 it ("'sub' function should return subtraction of input parameters", async () => {
-    expect( await Calculator.performOperation('sub', 'L,III,X,VI,I,IX')).toBe (21);
+    expect( await Calculator.performOperation('sub', 'L,III,X,VI,I,IX')).toBe ("XXI");
 });
 
 it ("'mult' function should return multiplication of input parameters", async () => {
-    expect( await Calculator.performOperation('mult', 'I,II,III,IV,V')).toBe (120);
+    expect( await Calculator.performOperation('mult', 'I,II,III,IV,V')).toBe ("CXX");
 });
 
 it ("'div' function should return multiplication of input parameters", async () => {
-    expect( await Calculator.performOperation('div', 'LX,III,II')).toBe (10);
+    expect( await Calculator.performOperation('div', 'LX,III,II')).toBe ("X");
+    expect( await Calculator.performOperation('div', 'XX,III')).toBe ("VI");
 });
 
 it ("tests to check if input includes only 1 operand", async () => {
