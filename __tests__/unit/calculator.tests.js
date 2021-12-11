fd5fd196cb75a40675c7f6e2fabbd410b74b84fc
maheshwarishikha <maheshwari.shikha@gmail.com>
2021-12-11 07:32:06 +0530

    Add files via upload

--- __tests__/unit/calculator.tests.js
+++ __tests__/unit/calculator.tests.js
@@ -9,25 +9,29 @@ it ("calculator function should accept two arguments", () => {
 });
 
 it ("calculator function should understand 'add', 'sub', 'mult' and 'div' operation", () => {
-    // expect( Calculator.performOperation('add', 'params')).not.toBe('Unsupported Operation');
-    expect( Calculator.performOperation('sub', 'params')).not.toBe('Unsupported Operation');
-    expect( Calculator.performOperation('mult', 'params')).not.toBe('Unsupported Operation');
-    expect( Calculator.performOperation('div', 'params')).not.toBe('Unsupported Operation');
-    expect( Calculator.performOperation('abc', 'params')).toBe ('Unsupported Operation');
+    // expect( Calculator.performOperation('add', 'I')).not.toBe('Unsupported Operation');
+    // expect( Calculator.performOperation('sub', 'I')).not.toBe('Unsupported Operation');
+    // expect( Calculator.performOperation('mult', 'II')).not.toBe('Unsupported Operation');
+    // expect( Calculator.performOperation('div', 'III')).not.toBe('Unsupported Operation');
+    // expect( Calculator.performOperation('abc', 'I, II')).toBe ('Unsupported Operation');
 });
 
-it ("'add' function should return addition of input parameters", () => {
-    expect( Calculator.performOperation('add', 'I, IV, X, XX')).toBe (35);
+it ("'add' function should return addition of input parameters", async () => {
+    expect( await Calculator.performOperation('add', 'I, IV, X, XX')).toBe (35);
 });
 
-it ("'sub' function should return subtraction of input parameters", () => {
-    expect( Calculator.performOperation('sub', 'L, III, X, VI, I, IX')).toBe (21);
+it ("'sub' function should return subtraction of input parameters", async () => {
+    expect( await Calculator.performOperation('sub', 'L, III, X, VI, I, IX')).toBe (21);
 });
 
-it ("'mult' function should return multiplication of input parameters", () => {
-    expect( Calculator.performOperation('mult', 'I, II, III, IV, V')).toBe (120);
+it ("'mult' function should return multiplication of input parameters", async () => {
+    expect( await Calculator.performOperation('mult', 'I, II, III, IV, V')).toBe (120);
 });
 
-it ("'div' function should return multiplication of input parameters", () => {
-    expect( Calculator.performOperation('div', 'LX, III, II')).toBe (10);
+it ("'div' function should return multiplication of input parameters", async () => {
+    expect( await Calculator.performOperation('div', 'LX, III, II')).toBe (10);
+});
+
+it ("tests to check if input includes only 1 operand", async () => {
+    expect( await Calculator.performOperation('add', 'I')).toBe ('I');
 });
